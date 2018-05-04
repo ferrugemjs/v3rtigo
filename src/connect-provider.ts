9c51de5b6ce6818685e54f770324b013f258fa1c
ferrugemjs <ferrugemjavascript@gmail.com>
2018-05-04 04:47:57 -0300

    feat: support to others events

--- src/connect-provider.ts
+++ src/connect-provider.ts
@@ -1,34 +1,38 @@
 import { IStore } from './i-store';
 
 export class ConnectProvider{
-  public stores:IStore<any>[];
+  public store:IStore<any>;
   private unSubs:Function[];
   private eventHandler: Function;
-  public stateChanged: { subscribe: Function};
+  public eventType: string;
+  public event: { subscribe: Function};
   constructor(){
-    this.stateChanged = {
-      subscribe: (eventHandler: Function) => {
+    this.event = {
+      handler: (eventHandler: Function) => {
         this.eventHandler = eventHandler;
       }
     };
   }
   private attached(){
-    if(this.eventHandler && this.stores){
+    if(this.eventHandler && this.store){
       this.unSubs = [];
-      this.stores.forEach( store => {
-        this.unSubs.push(store.subscribe('state:changed', this.eventHandler ));
-      });
+      //this.stores.forEach( store => {
+        this.unSubs.push(this.store.subscribe(this.eventType | 'state:changed', this.eventHandler ));
+      //});
     }
+    delete this.store;
+    delete this.eventHandler;
+    delete this.event;
   }
   private detached(){
     this.unSubs.forEach(unSub => unSub());
     this.unSubs.length = 0;
     this.unSubs = [];
-    this.stores.length = 0;
-    this.stores = [];
-    delete this.stateChanged;
-    delete this.eventHandler;
-    delete this.stores;
+    // this.stores.length = 0;
+    // this.stores = [];
+    // delete this.stateChanged;
+    // delete this.eventHandler;
+    // delete this.stores;
     delete this.unSubs;  
   }
 }
