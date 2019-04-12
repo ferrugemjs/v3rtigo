3bfcf1cfe23e61cf739c107dcf7cea061372ccf2
ferrugemjs <ferrugemjavascript@gmail.com>
2019-04-12 01:26:08 -0300

    fix: update html template script

--- src/connect-provider.html.js
+++ src/connect-provider.html.js
@@ -3,18 +3,24 @@
   <div class='connect-provider'></div>
 </template>
 */
-define(['exports','incremental-dom','./connect-provider'], function (exports,_idom,_connect_provider){
+define(["exports", "incremental-dom", "./connect-provider"], function(exports, _idom, _connect_provider) {
   var __connect_provider_tmp = Object.keys(_connect_provider)[0];
-  exports.default = (function (super_clazz){
-    function _clazz_sub_KGOTTM8iaEBdx1R6FLL4kC_tmp (){
-      if(super_clazz.call){
-        super_clazz.call(this);
+  exports.default = (function(super_clazz) {
+    function _clazz_sub_KGOTTM8iaEBdx1R6FLL4kC_tmp(props) {
+      if (super_clazz.call) {
+        super_clazz.call(this, props);
       }
-    }
-    _clazz_sub_KGOTTM8iaEBdx1R6FLL4kC_tmp.prototype = Object.create (super_clazz.prototype || super_clazz);
+    };
+    _clazz_sub_KGOTTM8iaEBdx1R6FLL4kC_tmp.prototype = Object.create(super_clazz.prototype || super_clazz);
     _clazz_sub_KGOTTM8iaEBdx1R6FLL4kC_tmp.prototype.constructor = _clazz_sub_KGOTTM8iaEBdx1R6FLL4kC_tmp;
-    _clazz_sub_KGOTTM8iaEBdx1R6FLL4kC_tmp.prototype._$attrs$_ = {'name':'div','static':['class','connect-provider'],'dinamic':'\'\''};
-    _clazz_sub_KGOTTM8iaEBdx1R6FLL4kC_tmp.prototype.render = function ($_this_$){  };
+    _clazz_sub_KGOTTM8iaEBdx1R6FLL4kC_tmp.prototype.$render = function(config_props) {
+      if (!config_props.loaded) {
+        _idom.elementOpen("fragment", "" + (config_props.key_id) + "", ["class", "connect-provider"], "is", (config_props.is), "id", (config_props.key_id));
+      };
+      if (!config_props.loaded) {
+        _idom.elementClose("fragment");
+      };
+    };
     return _clazz_sub_KGOTTM8iaEBdx1R6FLL4kC_tmp;
   })(_connect_provider[__connect_provider_tmp] || _connect_provider);
 });
